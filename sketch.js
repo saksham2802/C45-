1c8e5433520996ba1f3668da82496a1209479911
saksham2802 <63649583+saksham2802@users.noreply.github.com>
2021-06-22 21:43:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var canvas, backgroundImage;
+
+var gameState = 0,finishedPlayers;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+var Bird1,Bird2,Bird3,Bird4,Birds
+
+
+
+function preload(){
+  Bird1_img=loadImage("Images/Bird1.png")
+  Bird2_img=loadImage("Images/Bird2.jpg")
+  Bird3_img=loadImage("Images/Bird3.jpeg")
+  Bird4_img=loadImage("Images/Bird4.png")
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth , displayHeight);
+  database = firebase.database();
+  gameState = 0;
+  distance = 0;
+ // finishedPlayers = 0;
+  yVel = 0;
+  xVel = 0;
+
+  xSet = false;
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+   //start the game
+   background(200, 200, 255);
+
+   //start the game
+   if (playerCount === 4 ) {
+     game.update(1);
+   }
+ 
+   //start the game for real
+   if (gameState === 1) {
+     game.play();
+   }
+   if (gameState === 2) {
+     console.log("End");
+   }}
+ 
+  
